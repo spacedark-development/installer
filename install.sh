b3b5b37f00e81c57b2f360d15220c37b90620034
Damian Schönberger <schoenbergerdamian@outlook.de>
2024-08-31 08:54:57 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Clear the terminal screen
+clear
+
+# Print the header
+echo "#############################################"
+echo "#           Spacedark Development           #"
+echo "#############################################"
+echo ""
+
+# Display the menu options
+echo "Bitte wählen Sie eine Option:"
+echo "1) Uptime der Website holen"
+echo "2) Beenden"
+echo ""
+
+# Read user input
+read -p "Wählen Sie eine Option (1 oder 2): " option
+
+# Handle the user input
+case $option in
+    1)
+        echo "Uptime der Website wird geholt... In Arbeit!"
+        # Hier könnte ein Befehl zum Abrufen der Uptime hinzugefügt werden, z.B.:
+        # curl -s http://yourwebsite.com/uptime
+        ;;
+    2)
+        echo "Beenden..."
+        exit 0
+        ;;
+    *)
+        echo "Ungültige Option, bitte versuchen Sie es erneut."
+        ;;
+esac
