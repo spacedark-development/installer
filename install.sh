9da781fe7cd558d051810f61109720209f6da85b
Damian Schönberger <schoenbergerdamian@outlook.de>
2024-09-12 14:03:48 +0200

    New Select
    Added New Option to select

--- install.sh
+++ install.sh
@@ -12,7 +12,8 @@ echo ""
 # Display the menu options
 echo "Bitte wählen Sie eine Option:"
 echo "1) Uptime der Website holen"
-echo "2) Beenden"
+echo "2) Panel Installer"
+echo "3) Beenden"
 echo ""
 
 # Read user input
@@ -22,13 +23,17 @@ read -p "Wählen Sie eine Option (1 oder 2): " option
 case $option in
     1)
         echo "Uptime der Website wird geholt... In Arbeit!"
+        wait 2
+        echo "Funktion ungültig..."
         # Hier könnte ein Befehl zum Abrufen der Uptime hinzugefügt werden, z.B.:
         # curl -s http://yourwebsite.com/uptime
         ;;
-    2)
+    3)
         echo "Beenden..."
         exit 0
         ;;
+    2)
+        echo "Funktion Ungültig..."
     *)
         echo "Ungültige Option, bitte versuchen Sie es erneut."
         ;;
